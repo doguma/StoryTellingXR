dee649620cbfe3ea802fa3e75991d7f62bf4f818
Hannah Do <hannah.hj.do@gmail.com>
2021-10-21 23:59:27 +0900

    additional files

--- Assets/Hannah/scripts/Waypoints.cs
+++ Assets/Hannah/scripts/Waypoints.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
 public class Waypoints : MonoBehaviour
 {
 
@@ -15,12 +14,15 @@ public class Waypoints : MonoBehaviour
 
     public GameObject[] waypoints;
     int currentWP = 0;
-    bool isWalkingTowards = false;
+
     public Animator anim;
     public GameObject sittingpoint;
 
     public float speed = 2.0f;
     public float rotSpeed = 3.0f;
+
+    private float curTime;
+    private float pauseDuration = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -31,28 +33,41 @@ public class Waypoints : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 1)
-            
             currentWP++;
 
         if (currentWP >= waypoints.Length)
             currentWP = 0;
 
-        if (currentWP >= 1)
+        if (curTime == 0)
         {
-            //speed = 0;
-            //IdleTime();
-            //speed = 1;
-            anim.SetBool("flying", false);
-            anim.SetBool("jumping", true);
+            curTime = Time.time; // Pause over the Waypoint
+            if ((Time.time - curTime) >= pauseDuration)
+            {
+                curTime = 0;
+            }
+
+            if (currentWP >= 6 && currentWP < 12)
+            {
+
+                anim.SetBool("flying", false);
+                anim.SetBool("jumping", true);
+            }
+            else if (currentWP >= 12)
+            {
+                anim.SetBool("flying", true);
+                anim.SetBool("jumping", false);
+            }
         }
+        else
+        {
+            Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
 
-        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
 
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
 
-        this.transform.Translate(0, 0, speed * Time.deltaTime);
-        
-
+        }
     }
 }
